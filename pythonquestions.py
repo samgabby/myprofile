84e2665cc653ab16a8b0d1d3a6679b657048f798
samgabby <38702246+samgabby@users.noreply.github.com>
2018-05-02 23:55:18 +0100

    Add files via upload

---
+++ pythonquestions.py
@@ -0,0 +1,501 @@
+
+#BASIC CONCEPTS
+
+#FIRST PROGRAM
+#Print
+
+#SIMPLE OPERATIONS
+#Perform sums, multiplications, divisions, subtractions
+#Parentheses
+#Negatives
+#Cause zero division error
+
+#FLOATS
+#Floats
+
+#OTHER NUMERICAL OPERATIONS
+#Perform exponentiation, modulo, floor division
+
+#STRINGS
+#Make a string
+#Escape a character
+#Multi-line strings
+#Raw strings
+
+#SIMPLE INPUT AND OUTPUT
+#Input
+#Input with escape
+
+#STRING OPERATIONS
+#Add two strings
+#Add string and non-string
+#Repeat string
+
+#TYPE CONVERSION
+#Convert between variety of types
+#Take two inputs, convert to int, convert result float
+
+#VARIABLES
+#Assign a value to a variable and print it out
+#Simultaneously assign values to several variables
+#Multiply two or more variables
+#In succession, use same variable for two different types
+#Create a bad variable name
+#Reference non-existent variable
+#Delete variable
+#Take value of variable from user input
+
+#IN-PLACE OPERATORS
+#In-place operator
+#In-place operator to manipulate strings
+
+#CONTROL STRUCTURES
+
+#BOOLEANS AND COMPARISONS
+#Boolean
+#Boolean by comparing values
+#Print Boolean
+#All comparison operators 
+
+#IF STATEMENTS
+#If statement
+#Nested if statement
+
+#ELSE STATEMENTS
+#If-else statement
+#Chain of if-else statements
+#If-elif-else statement
+
+#BOOLEAN LOGIC
+#All Boolean logical operators
+
+#OPERATOR PRECEDENCE
+#Write operation to illustrate operator precedence
+
+#WHILE LOOPS
+#While loop
+#Infinite while loop
+#Break statement
+#Continue statement
+
+#LISTS
+#Create list
+#Print item from list
+#Print empty list
+#Create nested list
+#Index out of bounds of a list
+#Index a string
+#Index an integer
+
+#LIST OPERATIONS
+#Reassign list index
+#Add two or more lists
+#Repeat list
+#Check if value is in list
+#Check if value is not in list#
+
+#LIST FUNCTIONS
+#Find length of list
+#Find largest value in list
+#Find smallest value in list
+#Find position of first occurrence of item in list
+#Count quantity of an item in the list
+
+#LIST METHODS
+#Append item to list
+#Insert item into list
+#Extend list with another list
+#Remove item from list, remove
+#Remove item from list, pop
+#Reverse order of list's items
+
+#RANGE
+#Create range
+#List using range function, one argument
+#List using range function, two arguments
+#List using range function and interval argument
+
+#FOR LOOPS
+#Iterate through list using while loop
+#Iterate through list using for loop
+#Iterate through for loop using range function#
+
+#SIMPLE CALCULATOR
+#Calculator#
+
+#FUNCTIONS AND MODULES
+
+#FUNCTIONS
+#Functions without arguments#
+
+#FUNCTIONS WITH ARGUMENTS
+#Functions with arguments#
+
+#RETURNING FROM FUNCTIONS
+#Return values from functions
+
+#COMMENTS AND DOCSTRINGS
+#Comments
+#Docstrings
+
+#FUNCTIONS AS OBJECTS
+#Reassignment of a function
+#Functions as objects#
+
+#MODULES
+#Import modules
+#From modules import submodules
+#From modules import submodules as aliases
+
+#STANDARD LIBRARY AND PIP
+#Pip install library statement
+
+#EXCEPTIONS AND FILES#
+
+#EXCEPTIONS
+#Try/except statement
+#Try/except statement with multiple except blocks
+#Try/except statement with multiple exceptions per block
+#Try/except statement with no exception specified
+
+#FINALLY
+#Finally block with caught exceptions 
+#Finally block with uncaught exceptions
+
+#RAISING EXCEPTIONS
+#Raise exception
+#Raise exception inside try/except statement
+#Raise exception in if block
+#Raise exception with argument
+#Raise exception without arguments
+
+#ASSERTIONS
+#Assertion without arguments
+#Assertion with arguments
+
+#OPENING FILES
+#Open file
+#Open file using (‘file.txt’, r)
+#Open file using (‘file.txt’, w)
+#Open file using (‘file.txt’, a)
+#Open file using (‘file.txt’, rb)
+#Open file using (‘file.txt’, wb)
+#Open file using (‘file.txt’, ab)
+#Close file
+
+#READING FILES
+#Open and read file
+#Read only specific number of bytes from file
+#Read only specific number of lines from file each containing specific number of bytes
+#Find number of characters in file
+#Turn file into a list of lines using readlines() 
+#Print file one character at a time
+#Print file one line at a time
+
+#WRITING FILES
+#Write to file
+#Display number of bytes written to file
+
+#WORKING WITH FILES
+#try/finally can be used to close file but won't catch errors unless specified
+#with statement will close file but won't catch errors
+#with statement will close file, try/except will catch errors
+
+#MORE TYPES
+
+#NONE
+#Print(None == None)
+#Print empty line
+#Print a function that returns nothing
+
+#DICTIONARIES
+#Print and index dictionary
+#Index incorrect dictionary key, catch error with try/except block
+#Print empty dictionary
+#Dictionary with mutable key
+#Dictionary with repeated key
+#Reassign dictionary key
+#Append new key/value pair to dictionary
+
+#DICTIONARY KEYWORDS AND METHODS
+#find if a key is in or not in a dictionary
+#Dictionary get method
+#Dictionary items method
+#Dictionary pop method
+#Simultaneously pop and iterate through dictionary
+#Enumerate dictionary keys
+
+#TUPLES
+#Create tuple with parentheses
+#Create Tuple without parentheses
+#Index a tuple
+#Reassign tuple key
+#Print empty tuple
+
+#LIST SLICES
+#List slicing
+#List slicing featuring interval argument
+#Reverse list slicing
+
+#LIST COMPREHENSION 
+#List comprehension
+#List comprehension featuring conditional statement
+#‘Out of memory’ list comprehension
+
+#STRING FORMATTING
+#format method, unnamed arguments
+#format method, named arguments
+
+#STRING FUNCTIONS
+#Join
+#Split
+#Replace
+#Startswith
+#Endswith
+#Upper
+#Lower 
+#Title
+#Capwords
+#Capitalise
+#%f
+#%s
+#%i
+#%d
+#%x
+#%c
+
+#NUMERIC FUNCTIONS
+#Round #Remember, round is a in-built function, not a numeric method
+#Min
+#Max
+#Abs
+#Sum 
+
+#LIST FUNCTIONS
+#All
+#Any
+#Enumerate 
+
+#TEXT ANALYSER
+#Text analyser
+
+#FUNCTIONAL PROGRAMMING
+
+#INTRO
+#Functions as objects
+#Lower order function
+#Higher order function
+#Pure function
+#Impure function
+#Memoization
+
+#LAMBDAS
+#Named function 
+#Anonymous or Unnamed function
+#Higher order function containing anonymous function
+#Assign anonymous function to variable
+
+#MAPS AND FILTERS
+#Map function using named function
+#Map function using anonymous function
+#Filter function
+
+#GENERATORS
+#Iterate through a for generator
+#Iterate through a while generator
+#Convert a for generator to a list
+#Convert a while generator to a list
+#Infinite generator
+
+#DECORATORS
+#Decorator
+#Pre-pended decorator
+
+#RECURSION
+#Recursion
+#Infinite recursion
+#Indirect recursion
+
+#SETS
+#Set, curly braces
+#Set, set function
+#in set
+#len(set)
+#Index a set
+#Set.add
+#Set.remove
+#Set.pop
+#Union
+#Intersection
+#Difference
+#Symmetric difference
+#Advantages of data structures
+
+#ITERTOOLS
+#Count
+#Takewhile
+#Accumulate
+#Chain
+#Permutations, product
+
+#OBJECT ORIENTED PROGRAMMING
+
+#CLASSES
+#Class creation
+#Constructor creation
+#Class instance
+#The type of an instance
+#Instance attributes
+#'Instance' methods
+#Accessing instance attributes and 'instance' methods using an instance
+#Accessing 'instance' methods using a class name
+#Class attributes
+#AttributeError
+
+#INHERITANCE
+#Inheritance
+#overriding
+#indirect inheritance
+#super()
+
+#MAGIC METHODS
+#Magic Methods for arithmetic operators
+#Magic Methods for logical operators
+#Magic Methods for comparison operators
+#Magic Methods for container operation
+
+#OBJECT LIFECYCLE
+#how do you reduce the reference count of an object?
+
+#DATA HIDING
+#private method
+#special method
+#weakly private method
+#strongly private method
+#name mangling
+
+#CLASS AND STATIC METHODS
+#class method
+#factory method?
+#access a static method using the class name
+#access a static method using an instance of a class
+
+#PROPERTIES
+#property without setter facility
+#property with setter facility
+
+#A SIMPLE GAME
+#A simple game
+
+#REGULAR EXPRESSIONS
+
+#REGULAR EXPRESSIONS
+#re.match
+#re.search
+#re.findall
+#re.finditer
+#re.sub
+#re.search().group()
+#re.search().start()
+#re.search().end()
+#re.search().span()
+
+#METACHARACTERS
+#.
+#(..)
+#(.+)
+#^
+#$
+#(|)
+
+#CHARACTER CLASSES []
+#square brackets mean match any character
+#square brackets single range entire alphabet [A-Z]
+#square brackets single range numerals [0-9]
+#square brackets single range portion of alphabet [?-?]
+#square brackets single composite range [A-Za-z]
+#square brackets multiple range [A-Z][a-z]
+#^ for inverted character class
+
+#MORE METACHARACTERS
+#* 
+#+
+#?
+#{,}
+
+#GROUPS
+#groups and nested groups
+#match.group(0)
+#match.group(n)
+#match.groups()
+#?P<>
+#?:
+
+#SPECIAL SEQUENCES
+#\n, 0<n<100
+#\d, [0-9]
+#\s, [\t\n\r\f\v]
+#\w, [a-zA-Z0-9]
+#\D, non-digit
+#\S, non-whitespace
+#\W, non- word character
+
+#EMAIL EXTRACTION
+# r'([\w\.-]+)@([\w\.-]+)(\.[\w\.]+)'
+
+#PYTHONICNESS AND PACKAGING
+
+#THE ZEN OF PYTHON
+#import this
+
+#PEP
+#PEP 8
+#PEP 20
+#PEP 257
+
+#MORE ON FUNCTION ARGUMENTS
+# arbitratry arguments - *args, print(args)
+# default arguments
+# keyword arguments - **kwargs, print(kwargs)
+
+#TUPLE UNPACKING
+#tuple unpacking
+#variable swapping
+#starred assignment
+
+#TERNARY OPERATOR
+#Conditional expressions
+
+#MORE ON ELSE STATEMENTS
+#for...else
+#while...else
+#try except else
+
+#__MAIN__
+
+
+#MAJOR THIRD PARTY LIBRARIES
+#Django
+#CherryPy
+#Flask 
+#BeautifulSoup
+#matplotlib
+#NumPy 
+#SciPy 
+#pygame 
+#Panda3D
+
+#PACKAGING
+#Import Modules required for Packaging
+#Create directory structure
+#Write setup.py file
+#Build source diistribution
+#Build binary diistribution (executable installer)
+#Upload package
+#Install package
+
+
+#PACKAGING FOR USERS WITHOUT PYTHON INSTALLED
+#PyInstaller and cx_Freeze
+#py2exe
+#py2app 
